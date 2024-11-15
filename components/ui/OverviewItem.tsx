ba39a0a8591c2b77398b9a5a9fa5477d69ebb345
Nhung <hongnhung16052003@gmail.com>
2024-11-16 00:32:01 +0700

    build UI of the income page

--- components/ui/OverviewItem.tsx
+++ components/ui/OverviewItem.tsx
@@ -9,12 +9,15 @@ type overviewProps = {
 
 const OverviewItem: React.FC<overviewProps> = ({ title, type, number }) => {
   return (
-    <View className="flex-col items-center gap-y-1 p-3 pb-4 bg-[#FFEBE4] w-[31%] rounded-[6px]">
+    <View className="flex-col items-center gap-y-1 px-2 py-3 pb-4 bg-[#FFEBE4] w-[31%] rounded-[6px]">
       <View className="w-[80%] h-[1.5px] bg-[#FFF6F3]"></View>
       <Text className="text-[12px] text-[rgba(0,0,0,0.5)]">{title}</Text>
-      <Text className="text-[14px] text-[#D5420C] font-bold">
-        {type === "history" ? number + " chuyến" : number + " đồng"}
-      </Text>
+      <View className="flex-col items-center">
+        <Text className="text-[14px] text-[#D5420C] font-bold">
+          {type === "history" ? number + " chuyến" : number + ""}
+        </Text >
+        {type === "income" && <Text className="text-[14px] text-[#D5420C] font-bold">VNĐ</Text>}
+      </View>
     </View>
   );
 };
